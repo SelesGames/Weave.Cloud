b38c34fc874ecff0dc13e5b72c72444eab8316d1
selesgames <aemami99@gmail.com>
2012-11-08 18:39:28 -0800

    cleanup

--- Weave.Mobilizer.NinjectKernel/Kernel.cs
+++ Weave.Mobilizer.NinjectKernel/Kernel.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.WindowsAzure;
 using Ninject;
-using System.Threading.Tasks;
 using Weave.Mobilizer.Core.Cache;
 using Weave.Mobilizer.Core.Service;
 using Weave.Readability;
@@ -16,18 +15,12 @@ namespace Weave.Mobilizer.NinjectKernel
             ReadabilityClient rc = new ReadabilityClient("a142c0cc575c6760d5c46247f8aa6aabbacb6fd8");
             var azureClient = new AzureClient(
                 new StorageCredentialsAccountAndKey("weave", "uudFrra70qkI64bifaI2Rrm37CZ1HkzaBQrLMyw6U/hmzNDZehXeo9DdUv7BCpuZY4N2q/CNpNwYxW2fa218xA=="));
-                //new StorageCredentialsAccountAndKey("weavestorage", "04vqEHpHfPR1Lt8bvyvhd1p1jAWekHhemN0Mz58du/XJGfWY2OwRpG4972XCJzrOd+vO0d/rog1Alaw8HNUVHw=="));
+
             var azureCache = new AzureStorageCache(azureClient);
             var localCache = new LocalMemoryCache();
             var nLevelCache = new ReadabilityCache(rc, localCache, azureCache);
-            //var formatter = new HtmlFormatter();
 
-            Bind<ReadabilityClient>().ToConstant(rc).InSingletonScope();
-            Bind<AzureClient>().ToConstant(azureClient).InSingletonScope();
-            //Bind<AzureStorageCache>().ToConstant(azureCache).InSingletonScope();
-           // Bind<LocalMemoryCache>().ToConstant(localCache).InSingletonScope();
             Bind<ReadabilityCache>().ToConstant(nLevelCache).InSingletonScope();
-            //Bind<HtmlFormatter>().ToConstant(formatter).InSingletonScope();
         }
     }
 }
