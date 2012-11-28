5985e3b168fc14016af24fd59bb835c0af5cdc6c
selesgames <aemami99@gmail.com>
2012-11-28 01:22:12 -0800

    RssFeedParser

--- Weave.RssAggregator.Core/Parsing/RssFeedParser.cs
+++ Weave.RssAggregator.Core/Parsing/RssFeedParser.cs
@@ -8,7 +8,7 @@ using System.Xml.Linq;
 
 namespace Weave.RssAggregator.Core.Parsing
 {
-    internal static class RssFeedParser
+    public static class RssFeedParser
     {
         static readonly XNamespace atom = "http://www.w3.org/2005/Atom";
 
@@ -18,15 +18,39 @@ namespace Weave.RssAggregator.Core.Parsing
 
         static IEnumerable<IRssIntermediate> ParseUsingCustomParser(this Stream stream)
         {
-            var doc = XDocument.Load(stream, LoadOptions.None);
+            List<IRssIntermediate> elements = new List<IRssIntermediate>();
+            try
+            {
+                var doc = XDocument.Load(stream, LoadOptions.None);
 
-            if (doc.Root.Name == atom + "feed")
-                return doc.Descendants(atom + "entry").Select(o => new AtomIntermediate(o));
-            else
-                return doc.Descendants("item").Select(o => new Rss20Intermediate(o));
+                if (doc.Root.Name == atom + "feed")
+                    return doc.Descendants(atom + "entry").Select(o => new AtomIntermediate(o));
+                else
+                    return doc.Descendants("item").Select(o => new Rss20Intermediate(o));
+            }
+            catch { }
+            return elements;
         }
 
         #endregion
+
+        //static IEnumerable<XElement> GetChildContentElements(XmlReader reader)
+        //{
+        //    // move to first child
+        //    reader.Read();
+
+        //    while (true)
+        //    {
+        //        // skip whitespace between elements
+        //        reader.MoveToContent();
+
+        //        // break on end document section
+        //        if (reader.LocalName == "sectPr")
+        //            yield break;
+
+        //        yield return (XElement)XElement.ReadFrom(reader);
+        //    }
+        //}
 
 
 
@@ -35,10 +59,18 @@ namespace Weave.RssAggregator.Core.Parsing
 
         static IEnumerable<IRssIntermediate> ParseUsingSyndicationFeed(this Stream stream)
         {
-            using (XmlReader reader = XmlReader.Create(stream))
+            try
             {
-                var feed = SyndicationFeed.Load(reader);
-                return feed.Items.Select(item => new SyndicationIntermediate(item)).ToList();
+                using (XmlReader reader = XmlReader.Create(stream))
+                {
+                    var feed = SyndicationFeed.Load(reader);
+                    return feed.Items.Select(item => new SyndicationIntermediate(item)).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                DebugEx.WriteLine(ex.ToString());
+                return new List<IRssIntermediate>();
             }
         }
 
@@ -49,29 +81,32 @@ namespace Weave.RssAggregator.Core.Parsing
 
         public static IEnumerable<IRssIntermediate> ToRssIntermediates(this Stream stream)
         {
-            IEnumerable<IRssIntermediate> elements = null;
-
-            using (var ms = stream.ToMemoryStream())
+            try
             {
-                try
+                IEnumerable<IRssIntermediate> elements;
+
+                using (var ms = stream.ToMemoryStream())
                 {
                     elements = ParseUsingCustomParser(ms);
+
+                    if (elements != null && elements.Any())
+                        return elements;
+
+                    else
+                    {
+                        ms.Position = 0;
+                        elements = ParseUsingSyndicationFeed(ms);
+                    }
+
+                    ms.Close();
                 }
-                catch { }
 
-                if (elements != null && elements.Any())
-                    return elements;
-
-                else
-                {
-                    ms.Position = 0;
-                    elements = ParseUsingSyndicationFeed(ms);
-                }
-
-                ms.Close();
+                return elements ?? new List<IRssIntermediate>();
             }
-
-            return elements ?? new List<IRssIntermediate>();
+            catch (Exception)
+            {
+                return new List<IRssIntermediate>();
+            }
         }
 
         public static IEnumerable<Tuple<DateTime, IRssIntermediate>> ToRssIntermediatesWithDate(this IEnumerable<IRssIntermediate> elements)
