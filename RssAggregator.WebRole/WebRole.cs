3ef141906d51d37f28acf6963a1dbeb64591dbdf
selesgames <aemami99@gmail.com>
2013-02-20 21:55:07 -0800

    cleanup

--- RssAggregator.WebRole/WebRole.cs
+++ RssAggregator.WebRole/WebRole.cs
@@ -1,17 +1,10 @@
 using Microsoft.WindowsAzure.ServiceRuntime;
-using RssAggregator.WebRole.Startup;
 using System.Net;
 
 namespace RssAggregator.WebRole
 {
     public class WebRole : RoleEntryPoint
     {
-        //StartupTask startupTask;
-
-        //public WebRole()
-        //{
-        //    startupTask = new StartupTask();
-        //}
 
         public override bool OnStart()
         {
@@ -19,11 +12,6 @@ namespace RssAggregator.WebRole
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
 
             ServicePointManager.DefaultConnectionLimit = 12;
-
-            DebugEx.WriteLine(ServicePointManager.DefaultConnectionLimit);
-
-            //startupTask.OnStart();
-
             return base.OnStart();
         }
     }
