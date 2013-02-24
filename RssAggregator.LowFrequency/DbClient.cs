cb80a9ed75fa3f99f3ac2408682012abd436f7d3
selesgames <aemami99@gmail.com>
2013-02-24 00:30:59 -0800

    improvement

--- RssAggregator.LowFrequency/DbClient.cs
+++ RssAggregator.LowFrequency/DbClient.cs
@@ -1,70 +1,47 @@
 ﻿using Common.Data;
-using SelesGames.Common;
+using ProtoBuf;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Weave.RssAggregator.Core.DTOs.Outgoing;
-using Sql = RssAggregator.Data.Sql;
-using ProtoBuf;
-using System.IO;
 
 namespace Weave.RssAggregator.LowFrequency
 {
     public class DbClient
     {
-        IProvider<ITransactionalDatabaseClient> dbClientProvider;
+        SqlStoredProcClient storedProcClient;
 
         static DbClient()
         {
             ProtoBuf.Serializer.PrepareSerializer<NewsItem>();
         }
 
-        public DbClient(IProvider<ITransactionalDatabaseClient> dbClientProvider)
+        public DbClient(SqlStoredProcClient storedProcClient)
         {
-            this.dbClientProvider = dbClientProvider;
+            this.storedProcClient = storedProcClient;
         }
 
-        public async Task<List<NewsItem>> GetLatestNewsForFeedId(Guid feedId)
+        public async Task<List<NewsItem>> GetLatestNNewsAsBlobList(Guid id, int topCount = 25)
         {
-            using (var client = dbClientProvider.Get())
-            {
-                var news = await client.Get<Sql.NewsItem, NewsItem>(x => x
-                    .Where(o => feedId.Equals(o.FeedId))
-                    .OrderByDescending(o => o.PublishDateTime)
-                    .Take(25)
-                    .Select(o => new NewsItem
-                    {
-                        Title = o.Title,
-                        Link = o.Link,
-                        PublishDateTime = o.UtcPublishDateTimeString,
-                        ImageUrl = o.ImageUrl,
-                        VideoUri = o.VideoUri,
-                        YoutubeId = o.YoutubeId,
-                        PodcastUri = o.PodcastUri,
-                        ZuneAppId = o.ZuneAppId,
-                    }));
+            var sw = System.Diagnostics.Stopwatch.StartNew();
 
-                return news.ToList();
-            }
-        }
+            var storedProcName = "GetLatestNNewsAsBlobList";
 
-        public async Task<List<NewsItem>> GetLatestNewsForFeedIdViaBinary(Guid feedId)
-        {
-            using (var client = dbClientProvider.Get())
-            {
-                var news = await client.Get<Sql.NewsItem, System.Data.Linq.Binary>(x => x
-                    .Where(o => feedId.Equals(o.FeedId))
-                    .OrderByDescending(o => o.PublishDateTime)
-                    .Take(25)
-                    .Select(o => o.NewsItemBlob));
+            var rows = await storedProcClient.GetAsync<byte[]>(
+                storedProcName,
+                o => o.GetFieldValueAsync<byte[]>(0),
+                new SqlParameter("p1", id) { SqlDbType = System.Data.SqlDbType.UniqueIdentifier },
+                new SqlParameter("p2", topCount) { SqlDbType = System.Data.SqlDbType.Int });
 
-                var binaries = news.ToList();
-                var byteArrays = binaries.Select(o => o.ToArray());
-                var result = byteArrays.Select(FromByteArray).ToList();
+            var result = rows.OfType<byte[]>().Select(FromByteArray).ToList();
 
-                return result;
-            }
+            sw.Stop();
+            DebugEx.WriteLine("BINARY BLOB via stored proc SQL TOOK : {0} ms", sw.ElapsedMilliseconds);
+
+            return result;
         }
 
         NewsItem FromByteArray(byte[] array)
@@ -77,3 +54,67 @@ namespace Weave.RssAggregator.LowFrequency
         }
     }
 }
+
+
+
+
+#region deprecated
+
+
+//public async Task<List<NewsItem>> GetLatestNewsForFeedId(Guid feedId, int topCount = 25)
+//{
+//    var sw = System.Diagnostics.Stopwatch.StartNew();
+
+//    using (var client = dbClientProvider.Get())
+//    {
+//        var news = await client.Get<Sql.NewsItem, NewsItem>(x => x
+//            .Where(o => feedId.Equals(o.FeedId))
+//            .Where(o => o.NewsItemBlob != null)
+//            .OrderByDescending(o => o.PublishDateTime)
+//            .Take(25)
+//            .Select(o => new NewsItem
+//            {
+//                Title = o.Title,
+//                Link = o.Link,
+//                PublishDateTime = o.UtcPublishDateTimeString,
+//                ImageUrl = o.ImageUrl,
+//                VideoUri = o.VideoUri,
+//                YoutubeId = o.YoutubeId,
+//                PodcastUri = o.PodcastUri,
+//                ZuneAppId = o.ZuneAppId,
+//            }));
+
+//        var result = news.ToList();
+
+//        sw.Stop();
+//        DebugEx.WriteLine("REGULAR SQL TOOK : {0} ms", sw.ElapsedMilliseconds);
+
+//        return result;
+//    }
+//}
+
+//public async Task<List<NewsItem>> GetLatestNewsForFeedIdViaBinary(Guid feedId)
+//{
+//    var sw = System.Diagnostics.Stopwatch.StartNew();
+
+//    using (var client = dbClientProvider.Get())
+//    {
+//        var news = await client.Get<Sql.NewsItem, System.Data.Linq.Binary>(x => x
+//            .Where(o => feedId.Equals(o.FeedId))
+//            .Where(o => o.NewsItemBlob != null)
+//            .OrderByDescending(o => o.PublishDateTime)
+//            .Take(25)
+//            .Select(o => o.NewsItemBlob));
+
+//        var binaries = news.ToList();
+//        var byteArrays = binaries.Select(o => o.ToArray());
+//        var result = byteArrays.Select(FromByteArray).ToList();
+
+//        sw.Stop();
+//        DebugEx.WriteLine("BINARY BLOB SQL TOOK : {0} ms", sw.ElapsedMilliseconds);
+
+//        return result;
+//    }
+//}
+
+#endregion
