7f2386ab0dda7cb920394d251e0f06aa6d2d48c1
selesgames <aemami99@gmail.com>
2013-05-08 19:35:59 -0700

    added common.caching project

---
+++ Common.Caching/CacheEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Common.Caching
+{
+    public class CacheEntry<T>
+    {
+        public string CacheSourceName { get; set; }
+        public T Value { get; set; }
+        public DateTime LastAccess { get; set; }
+    }
+}
---
+++ Common.Caching/LocalMemoryCache.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace Common.Caching
+{
+    public class LocalMemoryCache<TKey, TResult> : IExtendedCache<TKey, TResult>
+    {
+        ConcurrentDictionary<TKey, CacheEntry<TResult>> cache = new ConcurrentDictionary<TKey, CacheEntry<TResult>>();
+        SerialDisposable timerDisposeHandle = new SerialDisposable();
+
+        public TimeSpan CacheTTL { get; private set; }
+
+        public TResult GetOrAdd(TKey key, Func<TKey, TResult> valueFactory)
+        {
+            //Debug.WriteLine(string.Format("attempting to get {0} from local cache", key), "LOCAL");
+            var entry = cache.GetOrAdd(
+                key,
+                _ =>
+                {
+                    //Debug.WriteLine(string.Format("{0} added to local cache", key), "LOCAL");
+                    var val = valueFactory(key);
+                    return new CacheEntry<TResult> { Value = val };
+                });
+            entry.LastAccess = DateTime.Now;
+            return entry.Value;
+        }
+
+        public virtual void OnExpire(TResult obj) { }
+
+
+
+
+        #region helper functions for clearing the cache
+
+        public void SetCacheTTLInMinutes(int minutes)
+        {
+            CacheTTL = TimeSpan.FromMinutes(minutes);
+            SubscribeToPulse();
+        }
+
+        void SubscribeToPulse()
+        {
+            timerDisposeHandle.Disposable = Observable.Interval(CacheTTL).Subscribe(notUsed => Pulse());
+        }
+
+        void Pulse()
+        {
+            var now = DateTime.Now;
+            var cacheEntries = cache.ToList();
+
+            foreach (var kvp in cacheEntries)
+            {
+                var entry = kvp.Value;
+                var key = kvp.Key;
+                var elapsed = now - entry.LastAccess;
+                if (elapsed > CacheTTL)
+                {
+                    CacheEntry<TResult> temp;
+                    if (cache.TryRemove(key, out temp))
+                        temp = null;
+
+                    OnExpire(entry.Value);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
