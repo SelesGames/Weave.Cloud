07d5f51e198481caba8848cba5fbba35ed9f83c9
selesgames <aemami99@gmail.com>
2013-05-29 15:14:23 -0700

    Moved AuthInfo.sql into its own project to reflect the sql server structure

--- Weave.Sql.Account/AuthInfo.sql
+++ Weave.Sql.Account/AuthInfo.sql
@@ -2,8 +2,13 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
 	[UserId] UNIQUEIDENTIFIER NOT NULL, 
+	[UserName] VARCHAR(MAX) NULL,
+	[PasswordHash] VARCHAR(MAX) NULL,
 	[FacebookAuthString] VARCHAR(MAX) NULL,
 	[TwitterAuthString] VARCHAR(MAX) NULL,
 	[MicrosoftAuthString] VARCHAR(MAX) NULL,
-	[GoogleAuthString] VARCHAR(MAX) NULL
+	[GoogleAuthString] VARCHAR(MAX) NULL,
 )
+
+GO
+CREATE UNIQUE INDEX [AuthInfoIndex] on [dbo].[AuthInfo] ([UserId])
