10208d4c9ab5a50860681fcb89974c87a38a324e
selesgames <aemami99@gmail.com>
2013-06-23 17:02:28 -0700

    added HtmlParser project

---
+++ Weave.Mobilizer.HtmlParser/Parser.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weave.Mobilizer.Client;
+
+namespace Weave.Mobilizer.HtmlParser
+{
+    public class Parser
+    {
+        string url = "http://www.wpcentral.com/e-online-releases-official-app-windows-phone-8";
+
+        public async void TryStuff()
+        {
+            var dto = await new MobilizerServiceClient("hxyuiplkx78!ksdfl").Get(url);
+            var html = dto.content;
+
+            var doc = new HtmlDocument();
+            doc.OptionDefaultStreamEncoding = Encoding.UTF8;
+            doc.Load(result);
+        }
+    }
+}
