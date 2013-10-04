4abe75df346303672e2623176836881a57fc6e72
Arash Emami <aemami99@gmail.com>
2013-10-04 10:07:03 -0700

    changes relating to service bus FeedUpdateNotice.cs

---
+++ RssAggregator.LowFrequency/FeedUpdateNotice.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Threading.Tasks;
+
+namespace Weave.RssAggregator.LowFrequency
+{
+    public class FeedUpdateNotice
+    {
+        BrokeredMessage message;
+
+        public FeedUpdateNotice(BrokeredMessage message)
+        {
+            this.message = message;
+        }
+
+        public string MessageId { get; set; }
+        public Guid FeedId { get; set; }
+        public DateTime RefreshTime { get; set; }
+
+        public Task MarkNoticeAsRead()
+        {
+            return message.CompleteAsync();
+        }
+    }
+}
