c029cd4cfbcce73d4bec0f5351cf0949e02618ca
Arash Emami <aemami99@gmail.com>
2013-10-05 20:21:37 -0700

    added Common.Azure.Logging project, and downloaded latest WindowsAzure.Storage package from NuGet

---
+++ Common.Azure.Logging/LogClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Azure.Logging
+{
+    public class LogClient
+    {
+        AzureTableClient tableClient;
+
+        public LogClient(string storageAccountName, string key, bool useHttps, string tableName)
+        {
+            tableClient = new AzureTableClient(storageAccountName, key, useHttps, tableName);
+        }
+
+        public async void Log(
+    }
+}
