fd02e62788b5b1c6c737976348393d279efc7d61
Arash Emami <aemami99@gmail.com>
2013-10-28 14:48:24 -0700

    removed GZip compression on UserInfoCacheSerializer

--- Weave.User.Service.Cache/UserInfoCacheSerializer.cs
+++ Weave.User.Service.Cache/UserInfoCacheSerializer.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.ApplicationServer.Caching;
 using ProtoBuf;
 using System.IO;
-using System.IO.Compression;
+//using System.IO.Compression;
 using Weave.User.DataStore;
 
 namespace Weave.User.Service.Cache
@@ -10,19 +10,22 @@ namespace Weave.User.Service.Cache
     {
         public object Deserialize(Stream stream)
         {
-            using (var gzipStream = new GZipStream(stream, CompressionMode.Decompress, true))
-            {
-                return Serializer.Deserialize<UserInfo>(gzipStream);
-            }
+            //using (var gzipStream = new GZipStream(stream, CompressionMode.Decompress, true))
+            //{
+            //    return Serializer.Deserialize<UserInfo>(gzipStream);
+            //}
+
+            return Serializer.Deserialize<UserInfo>(stream);
         }
 
         public void Serialize(Stream stream, object value)
         {
-            using (var gzipStream = new GZipStream(stream, CompressionMode.Compress, true))
-            {
-                var user = (UserInfo)value;
-                Serializer.Serialize<UserInfo>(gzipStream, user);
-            }
+            //using (var gzipStream = new GZipStream(stream, CompressionMode.Compress, true))
+            //{
+            //    var user = (UserInfo)value;
+            //    Serializer.Serialize<UserInfo>(gzipStream, user);
+            //}
+            Serializer.Serialize<UserInfo>(stream, (UserInfo)value);
         }
     }
 }
