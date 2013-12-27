41db95687da9cd02369198d9d45271de624aff14
Arash Emami <aemami99@gmail.com>
2013-12-27 01:55:09 -0800

    added stuff

---
+++ Weave.User.Paging/PagedNewsHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weave.User.BusinessObjects;
+
+namespace Weave.User.Paging
+{
+    public class PagedNewsHelper
+    {
+        UserInfo user;
+
+        public async Task DoStuff()
+        {
+            var now = DateTime.UtcNow;
+
+            await user.RefreshAllFeeds();
+
+            //var feedsWithNew = user.Feeds.AllNews().Where(o => o.)
+        }
+    }
+}
