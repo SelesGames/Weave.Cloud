bd0e4fdf0a2976a171ad567b1fa60a8d447dcfac
Arash Emami <aemami99@gmail.com>
2014-01-01 16:15:36 -0800

    improvements

--- Weave.User.Paging.AzureCloudService.WorkerRole/UserPagedNewsUpdater.cs
+++ Weave.User.Paging.AzureCloudService.WorkerRole/UserPagedNewsUpdater.cs
@@ -1,5 +1,7 @@
 ﻿using Common.Azure.SmartBlobClient;
 using Microsoft.WindowsAzure.Storage;
+using SelesGames.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,24 +13,48 @@ namespace Weave.User.Paging
 {
     public class UserPagedNewsUpdater
     {
-        readonly UserInfo user;
-        readonly SmartBlobClient client;
+        #region Private member variables
+
+        readonly Guid userId;
+        readonly SmartBlobClient userClient;
+        readonly SmartBlobClient pageClient;
         readonly string containerName;
 
         readonly int PAGE_SIZE = 50;
         readonly string MASTER_LIST = "masterlist";
+        readonly string USER_CONTAINER = "user";
+        readonly string USER_APPEND = ".user";
 
-        public UserPagedNewsUpdater(UserInfo user, CloudStorageAccount csa)
+        #endregion
+
+
+
+
+        #region Constructor
+
+        public UserPagedNewsUpdater(
+            Guid userId,
+            CloudStorageAccount userStorageAccount, 
+            CloudStorageAccount pageStorageAccount)
         {
-            this.user = user;
-            this.client = new SmartBlobClient(csa);
-            this.containerName = user.Id.ToString("N");
+            this.userId = userId;
+            this.userClient = new SmartBlobClient(userStorageAccount);
+            this.pageClient = new SmartBlobClient(pageStorageAccount);
+            this.containerName = userId.ToString("N");
         }
+
+        #endregion
+
+
+
 
         public async Task UpdateUsersPagedLists()
         {
+            // get the user.  If no use matches this id, throw an exception
+            var user = await GetUser();
+
             // create the container for this User if it doesn't already exist
-            var container = client.GetContainerHandle(containerName);
+            var container = pageClient.GetContainerHandle(containerName);
             await container.CreateIfNotExistsAsync();
 
 
@@ -62,7 +88,7 @@ namespace Weave.User.Paging
             var fileName = pagedNews.CreateFileName();
             var store = pagedNews.CreateSerializablePagedNews();
 
-            await client.Save(containerName, fileName, store);
+            await pageClient.Save(containerName, fileName, store);
         }
 
         async Task Save(MasterListsInfo masterList)
@@ -71,7 +97,7 @@ namespace Weave.User.Paging
             {
                 UserId = masterList.UserId,
             };
-            await client.Save(containerName, MASTER_LIST, masterList);
+            await pageClient.Save(containerName, MASTER_LIST, masterList);
         }
 
         #endregion
@@ -81,11 +107,19 @@ namespace Weave.User.Paging
 
         #region Get functions
 
+        async Task<UserInfo> GetUser()
+        {
+            var blobName = string.Format("{0}{1}", userId, USER_APPEND);
+            var storedUser = await userClient.Get<DataStore.UserInfo>(USER_CONTAINER, blobName);
+            var converter = Service.Converters.DataStoreToBusinessObject.Instance;
+            return storedUser.Convert<DataStore.UserInfo, UserInfo>(converter);
+        }
+
         async Task<MasterListsInfo> GetMasterListsInfo()
         {
             try
             {
-                var masterListsInfo = await client.Get<MasterListsInfo>(containerName, MASTER_LIST);
+                var masterListsInfo = await pageClient.Get<MasterListsInfo>(containerName, MASTER_LIST);
                 return masterListsInfo;
             }
             catch
