46e704c8e44a8cf31f6d1f6eb068f72d077d9c25
Arash Emami <aemami99@gmail.com>
2014-04-07 20:14:28 -0700

    added 2 more mediators to Weave.User.BusinessObjects

---
+++ Weave.User.BusinessObjects.v2/UserArticleStateExtendedMediator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Weave.User.BusinessObjects.v2
+{
+    public class UserArticleStateExtendedMediator
+    {
+        NewsItemStateCache newsItemStateCache;
+        UserInfo user;
+
+        public UserArticleStateExtendedMediator(UserInfo user, NewsItemStateCache newsItemStateCache)
+        {
+            this.user = user;
+            this.newsItemStateCache = newsItemStateCache;
+        }
+
+        public void MarkCategorySoftRead(string category)
+        {
+            var newsItemIds = user.Feeds
+                .FindByCategory(category)
+                .SelectMany(o => o.NewsItemIds);
+
+            var states = newsItemStateCache.MatchingIds(newsItemIds);
+
+            foreach (var state in states)
+            {
+                state.HasBeenViewed = true;
+            }
+        }
+
+        public void MarkFeedSoftRead(Guid feedId)
+        {
+            var newsItemIds = user.Feeds
+                .FindById(feedId)
+                .SelectMany(o => o.NewsItemIds);
+
+            var states = newsItemStateCache.MatchingIds(newsItemIds);
+
+            foreach (var state in states)
+            {
+                state.HasBeenViewed = true;
+            }
+        }
+    }
+}
