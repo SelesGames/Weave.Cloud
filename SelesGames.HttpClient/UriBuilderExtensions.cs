4e97b89b8b014763490bb33a91fae4018750611b
Arash Emami <aemami99@gmail.com>
2014-07-22 09:30:21 -0700

    cleanup of references

--- SelesGames.HttpClient/UriBuilderExtensions.cs
+++ SelesGames.HttpClient/UriBuilderExtensions.cs
@@ -6,7 +6,7 @@ namespace System
     {
         public static UriBuilder AddParameter(this UriBuilder uri, string parameterName, object parameterValue)
         {
-            var encodedQuery = string.Format("{0}={1}", parameterName, HttpUtility.UrlEncode(parameterValue.ToString()));
+            var encodedQuery = string.Format("{0}={1}", parameterName, Uri.EscapeDataString(parameterValue.ToString()));
 
             if (uri.Query != null && uri.Query.Length > 1)
                 uri.Query = uri.Query.Substring(1) + "&" + encodedQuery;
