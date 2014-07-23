098d932ab0e7360bf2381265ca0f76f4646e6a05
Arash Emami <aemami99@gmail.com>
2014-07-23 15:01:13 -0700

    more self host changes

---
+++ SelesGames.WebApi.SelfHost/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+
+namespace SelesGames.WebApi.SelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = SelfHost.Config;
+            app.UseWebApi(config);
+        }
+    }
+}
