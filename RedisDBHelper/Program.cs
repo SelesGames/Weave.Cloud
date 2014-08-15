4359b5fdc20dd4f06ec71e4d709426a715ae6303
Arash Emami <aemami99@gmail.com>
2014-08-15 04:10:03 -0700

    added RedisDBHelper project

---
+++ RedisDBHelper/Program.cs
@@ -0,0 +1,30 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDBHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stuff();
+        }
+
+        static void Stuff()
+        {
+            var redisClientConfig = ConfigurationOptions.Parse(
+"weaveuser.redis.cache.windows.net,ssl=false,password=dM/xNBd9hB9Wgn3tPhkTsiwzIw4gImnS+eAN9sYuouY=");
+
+            redisClientConfig.AllowAdmin = true;
+            var connectionMultiplexer = ConnectionMultiplexer.Connect(redisClientConfig);
+
+            var server = connectionMultiplexer.GetServer(
+"weaveuser.redis.cache.windows.net", 6379);
+            server.FlushDatabase(2);
+        }
+    }
+}
